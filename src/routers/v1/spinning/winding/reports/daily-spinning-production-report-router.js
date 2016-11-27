41079856e0199fc7593deaa29e826d2d8089cc39
Kanisius Andrew <kanisius.andrew@moonlay.com>
2016-11-27 23:30:44 +0700

    add firstday and lastly

--- src/routers/v1/spinning/winding/reports/daily-spinning-production-report-router.js
+++ src/routers/v1/spinning/winding/reports/daily-spinning-production-report-router.js
@@ -14,9 +14,11 @@ router.get("/", passport, function (request, response, next) {
     db.get().then(db => {
 
         var manager = new DailySpinningProductionReportManager(db, request.user);
+        var firstDay = request.params.firstDay;
+        var lastDay = request.params.lastDay;
         var unitId = request.params.unitId;
 
-        manager.getDailySpinningProductionReport(unitId)
+        manager.getDailySpinningProductionReport(firstDay, lastDay, unitId)
             .then(data => {
                 var result = resultFormatter.ok(apiVersion, 200, data);
                 response.send(200, result);
