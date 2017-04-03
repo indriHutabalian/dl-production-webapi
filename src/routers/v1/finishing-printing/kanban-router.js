c20942e3f3d85e08b4c1273f6fbc6c99efe316df
Tris Setiawan <tris.setiawan@yahoo.com>
2017-04-03 04:43:42 +0000

    support select fields on single/first query

--- src/routers/v1/finishing-printing/kanban-router.js
+++ src/routers/v1/finishing-printing/kanban-router.js
@@ -5,19 +5,29 @@ var db = require("../../../db");
 const apiVersion = '1.0.0';
 
 var handlePdfRequest = function(request, response, next) {
-    var filename = request.kanban.productionOrder.orderNo + " - " + request.kanban.cart.cartNumber;
-    request.manager.pdf(request.kanban)
-        .then(kanbanDocBinary => {
-            response.writeHead(200, {
-                'Content-Type': 'application/pdf',
-                'Content-Disposition': `attachment; filename=${filename}.pdf`,
-                'Content-Length': kanbanDocBinary.length
-            });
-            response.end(kanbanDocBinary);
+    var user = request.user;
+    var id = request.params.id;
+    var manager;
+    db.get()
+        .then(db => {
+            manager = new Manager(db, user);
+            return manager.getSingleByIdOrDefault(id);
         })
-        .catch(e => {
-            var error = resultFormatter.fail(apiVersion, 400, e);
-            response.send(400, error);
+        .then((kanban) => {
+            var filename = kanban.productionOrder.orderNo + " - " + kanban.cart.cartNumber;
+            manager.pdf(kanban)
+                .then(kanbanDocBinary => {
+                    response.writeHead(200, {
+                        'Content-Type': 'application/pdf',
+                        'Content-Disposition': `attachment; filename=${filename}.pdf`,
+                        'Content-Length': kanbanDocBinary.length
+                    });
+                    response.end(kanbanDocBinary);
+                })
+                .catch(e => {
+                    var error = resultFormatter.fail(apiVersion, 400, e);
+                    response.send(400, error);
+                });
         });
 };
 
@@ -30,45 +40,18 @@ function getRouter() {
     });
 
     var route = router.routes["get"].find(route => route.options.path === "/:id");
+    var originalHandler = route.handlers[route.handlers.length - 1];
     route.handlers[route.handlers.length - 1] = function(request, response, next) {
-        var manager;
-
-        db.get()
-            .then(db => {
-                manager = new Manager(db, request.user);
-                return manager.getSingleByIdOrDefault(request.params.id);
-            })
-            .then((kanban) => {
-                var isPDFRequest = (request.headers.accept || '').toString().indexOf("application/pdf") >= 0;
-                if (isPDFRequest){
-                    request.manager = manager;
-                    request.kanban = kanban;
-                    next();
-                }
-                else{
-                    var result = getResult(kanban);
-                    response.send(result.statusCode, result);
-                }
-            })
-            .catch((e) => {
-                var statusCode = 500;
-                if (e.name === "ValidationError")
-                    statusCode = 400;
-                var error = resultFormatter.fail(apiVersion, statusCode, e);
-                response.send(statusCode, error);
-            });
+        var isPDFRequest = (request.headers.accept || '').toString().indexOf("application/pdf") >= 0;
+        if (isPDFRequest) {
+            next();
+        }
+        else {
+            originalHandler(request, response, next);
+        }
     };
     route.handlers.push(handlePdfRequest);
     return router;
-}
-
-function getResult(kanban){
-    var result;
-    if (!kanban)
-        result = resultFormatter.fail(apiVersion, 404, new Error("data not found"));
-    else 
-        result = resultFormatter.ok(apiVersion, 200, kanban);
-    return result;
 }
 
 module.exports = getRouter;
